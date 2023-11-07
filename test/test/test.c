5c6399dc0a2d9f3bc2d8f0cc82525c51fcb001e4
111222kkk <148615978+111222kkk@users.noreply.github.com>
2023-11-07 19:40:51 +0800

    Add files via upload

---
+++ test/test/test.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main() {
+//	int a=0, b=0;
+//	scanf("%d %d", &a, &b);
+//	int sum = a + b;
+//	printf("%d",sum);
+//	return 0;
+//}
+//#include<stdio.h>
+//enum color 
+//{
+//	RED,
+//	BLUE,
+//	GREEN
+//};
+//
+//int main() {
+//	enum cocor c = GREEN;
+//	printf("%d",c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main() {
+//	printf("\a");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main() {
+//	char ch;
+//	printf("输入：");
+//	ch = getchar();
+//	ch = ch + 32;
+//	printf("输出：");
+//	putchar(ch);
+//	return 0;
+//}
+//#include<stdio.h>
+////构造函数
+//int get_max(int a, int b) 
+//{
+//	if (a > b) 
+//		return a;
+//	else 
+//		return b;
+//}
+//int main() {
+//	int num1;
+//	int num2;
+//	scanf("%d %d",&num1,&num2); 
+//	int max = get_max(num1,num2);
+//	printf("%d",max);
+//	return 0;
+//}
+//struct student
+//{
+//	int age->;
+//};
+//#include<stdio.h>
+//int main(){
+//    int M;
+//    scanf("%d",&M);
+//    if ((M % 5 == 0)) {
+//        printf("YES");
+//    }
+//    else {
+//        printf("NO");
+//    }
+//    return 0;
+//}
